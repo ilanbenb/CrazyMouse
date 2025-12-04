c9b6e9c2dc10ffc2fd5de37066b7d72f1495eb3e
ilanbenb <12747039+ilanbenb@users.noreply.github.com>
2025-12-04 23:08:20 +0200

    From prank to Nightmare

--- crazy_mouse.py
+++ crazy_mouse.py
@@ -6,9 +6,10 @@ import random
 if __name__ == '__main__':
     pyautogui.FAILSAFE = False
     while True:
-        random_time = random.randint(0, 12)
+        random_time = random.randint(0, 1200)
         time.sleep(random_time)
         random_x = random.randint(-30, 30)
         random_y = random.randint(-30, 30)
         duration = random.randint(2, 6)
+
         pyautogui.dragRel(random_x, random_y, duration=duration)
